5d78d9b6c715664d38b2acf3db8a125ad04a897c
luca-3 <mikeneuner441@gmail.com>
2022-04-10 19:45:33 +0200

    add objekts

--- src/main/java/com/example/game/main/main.java
+++ src/main/java/com/example/game/main/main.java
@@ -4,32 +4,47 @@ package com.example.game.main;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
 
 public class main extends Application {
+    Group root = new Group();
+    Scene scene = new Scene(root);
+    Stage stage = new Stage();
+
     public static void main(String[] args) {
         launch(args);
     }
 
     public void start(Stage primaryStage) throws Exception {
-        Group root = new Group();
-        Scene scene = new Scene(root);
-        Stage stage = new Stage();
+
 
         Text score = new Text(100, 100, "Score: 200");
         root.getChildren().add(score);
+
+        Line line = new Line(100, 100, 200, 200);
+        line.rotateProperty().set(45);
+        root.getChildren().add(line);
+
+        Rectangle rectangle = new Rectangle(100, 100, 100, 100);
+        rectangle.setFill(Color.RED);
+        root.getChildren().add(rectangle);
+
 
 
         stage.setTitle("Game");
         stage.setFullScreen(true);
 
-
         stage.setScene(scene);
         stage.show();
     }
+
+
 
 }
 
