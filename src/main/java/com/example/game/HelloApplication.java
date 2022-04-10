c52205d4814d503fda2ceefddb905197bc043b93
David <dav.joechner@gmail.com>
2022-04-10 14:42:00 +0200

    123

--- src/main/java/com/example/game/HelloApplication.java
+++ src/main/java/com/example/game/HelloApplication.java
@@ -6,7 +6,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-//test
+//test 12345678790
 
 public class HelloApplication extends Application {
     @Override
