5fe17594db740773330fb54fbadc2a7a13bbe5db
luca-3 <mikeneuner441@gmail.com>
2022-04-10 20:41:54 +0200

    v1 object movement

--- src/main/java/com/example/game/main/main.java
+++ src/main/java/com/example/game/main/main.java
@@ -4,50 +4,82 @@ package com.example.game.main;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import javax.swing.table.TableRowSorter;
 
 
 public class main extends Application {
     Group root = new Group();
     Scene scene = new Scene(root);
     Stage stage = new Stage();
+    Rectangle rectangle = new Rectangle(100, 100, 100, 100);
 
     public static void main(String[] args) {
         launch(args);
+
+
     }
 
     public void start(Stage primaryStage) throws Exception {
 
-
-        Text score = new Text(100, 100, "Score: 200");
-        root.getChildren().add(score);
-
-        Line line = new Line(100, 100, 200, 200);
-        line.rotateProperty().set(45);
-        root.getChildren().add(line);
-
         Rectangle rectangle = new Rectangle(100, 100, 100, 100);
         rectangle.setFill(Color.RED);
         root.getChildren().add(rectangle);
-
-
 
         stage.setTitle("Game");
         stage.setFullScreen(true);
 
         stage.setScene(scene);
         stage.show();
+
+        scene.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> {
+            if (key.getCode() == KeyCode.W) {
+                updatePosition(0, -10, rectangle);
+            } else if (key.getCode() == KeyCode.S) {
+                updatePosition(0, 10, rectangle);
+            } else if (key.getCode() == KeyCode.A) {
+                updatePosition(-10, 0, rectangle);
+            } else if (key.getCode() == KeyCode.D) {
+                updatePosition(10, 0, rectangle);
+            }
+        });
+    }
+
+    public void controls() {
+        scene.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> {
+            if (key.getCode() == KeyCode.W) {
+                updatePosition(0, -10, rectangle);
+            } else if (key.getCode() == KeyCode.S) {
+                updatePosition(0, 10, rectangle);
+            } else if (key.getCode() == KeyCode.A) {
+                updatePosition(-10, 0, rectangle);
+            } else if (key.getCode() == KeyCode.D) {
+                updatePosition(10, 0, rectangle);
+            }
+        });
+    }
+
+    public void updatePosition(int x, int y, Rectangle rectangle) {
+        int oldX = (int) rectangle.getX();
+        int oldY = (int) rectangle.getY();
+        rectangle.setX(oldX+x);
+        rectangle.setY(oldY+y);
+        update();
+    }
+
+    public void update(){
+        stage.show();
     }
 
 
 
+
+
 }
-
-
-
-
