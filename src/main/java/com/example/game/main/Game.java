e64c23d4e1858dc4e87d5e5d68e359457f1dcabc
luca-3 <mikeneuner441@gmail.com>
2022-04-10 17:23:41 +0200

    teststuff

---
+++ src/main/java/com/example/game/main/Game.java
@@ -0,0 +1,23 @@
+package com.example.game.main;
+
+import com.example.game.test.HelloApplication;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Game {
+
+    //Methode to initialize the game
+    public void start() {
+
+        System.out.println("Game started");
+        VBox vBox = new VBox(new Label("A JavaFX Label"));
+        Scene scene = new Scene(vBox);
+
+        Stage stage = new Stage();
+        stage.setScene(scene);
+
+    }
+}
