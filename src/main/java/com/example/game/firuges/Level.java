d04681593aa8305a82edb1de469706a19b3299ed
luca-3 <mikeneuner441@gmail.com>
2022-05-03 13:08:15 +0200

    level genaration

--- src/main/java/com/example/game/firuges/Level.java
+++ src/main/java/com/example/game/firuges/Level.java
@@ -1,17 +1,20 @@
 package com.example.game.firuges;
 
+import com.example.game.main.EnemyHandler;
+
 import java.util.List;
 import java.util.Random;
 
 public enum Level {
-        TESTLEVEL(2, 1, 1, 0, 0),
-        LEVEL_ONE(0,2,2,0,0),
-        LEVEL_TWO(0,2,2,0,0),
-        LEVEL_THREE(0,2,2,0,0),
-        LEVEL_FOUR(0,2,2,0,0),
-        LEVEL_FIVE(0,2,2,0,0),
-        LEVEL_SIX(0,2,2,0,0),
-        LEVEL_SEVEN(0,2,2,0,0),
+        //TESTLEVEL(2, 1, 1, 0, 0),
+        //LEVEL_ONE(0,2,2,0,0),
+        //LEVEL_TWO(0,2,2,0,0),
+        RAND(),
+        RAND1(),
+        RAND2(),
+
+
+
     ;
     /*
     So machst du ein Level Enum:
@@ -27,12 +30,36 @@ public enum Level {
     private final int puffy;
     private final int kitty;
 
+    Level(){
+        int[] numberOfEnemy = generateLevel();
+        this.affe = numberOfEnemy[0];
+        this.unicorn = numberOfEnemy[1];
+        this.dwarf = numberOfEnemy[2];
+        this.puffy = numberOfEnemy[3];
+        this.kitty = numberOfEnemy[4];
+    }
+
     Level(int affe, int unicorn, int dwarf, int puffy, int kitty){
+
         this.affe = affe;
         this.unicorn = unicorn;
         this.dwarf = dwarf;
         this.puffy = puffy;
         this.kitty = kitty;
+    }
+
+    public int[] generateLevel(){
+        Random r = new Random();
+        int numberEnemys = (int) (Math.sqrt(EnemyHandler.level - 1) * 2.5 + 2); //formula to calculate the number of enemy dependent on the current level
+        int[] enumInput = new int[5]; //number of Enemy, wich exists and can be an input of the Level enum
+
+        for (int i = numberEnemys; i > 0; i--){
+            int position = r.nextInt(0,enumInput.length - 1);
+            enumInput[position] += 1;
+        }
+        System.out.println("rand");
+
+        return enumInput;
     }
 
     public int getNumberAffe(){return affe;}
@@ -48,4 +75,6 @@ public enum Level {
     public static Level randomLevel()  {
         return VALUES.get(RANDOM.nextInt(SIZE));
     }
+
+
 }
