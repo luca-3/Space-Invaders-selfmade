7255ff8e256890ce347aaaee3f370f12cc8330fb
David <dav.joechner@gmail.com>
2022-05-03 13:04:54 +0200

    Zwerg

--- src/main/java/com/example/game/firuges/EnemyE.java
+++ src/main/java/com/example/game/firuges/EnemyE.java
@@ -8,7 +8,7 @@ import java.util.List;
 public enum EnemyE {
     AFFE(100, 200, 3,  2, MobMoveE.HDMOVE,"src/main/resources/com/example/game/enemy/Spaceship.png"),
     BANANE(40, 40, 1, 7, MobMoveE.NMOVE, "src/main/resources/com/example/game/enemy/Bannane1.png"),
-    UNICORN(64, 64, 2 , 2, MobMoveE.VERMOVE, "src/main/resources/com/example/game/enemy/Test.png"),
+    UNICORN(64, 64, 2 , 4, MobMoveE.VERMOVE, "src/main/resources/com/example/game/enemy/Test.png"),
     DWARF(75, 125, 1, 3, MobMoveE.RAINBOW, null),
     PUFFY(50, 50, 1, 6, MobMoveE.NMOVE, null),
     KITTY(75, 75, 1, 2, MobMoveE.NMOVE, null),
