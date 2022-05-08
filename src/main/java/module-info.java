48197a1e0d0648f10899269f7dca30fd94a07be8
luca-3 <mikeneuner441@gmail.com>
2022-05-08 14:01:47 +0200

    del unused files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,12 +4,8 @@ module com.example.game {
     requires java.desktop;
 
 
-    opens com.example.game to javafx.fxml;
-    exports com.example.game;
     exports com.example.game.figures;
     opens com.example.game.figures to javafx.fxml;
-    opens com.example.game.test to javafx.fxml;
-    exports com.example.game.test;
     exports com.example.game.main;
     opens com.example.game.main to javafx.fxml;
 }
