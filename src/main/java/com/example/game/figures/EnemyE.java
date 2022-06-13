dc28776dec88d606faa28337a07284b746f21890
Adni <adnibulliq@gmail.com>
2022-06-13 17:55:47 +0200

    add Method Interface in Enum --> not working

--- src/main/java/com/example/game/figures/EnemyE.java
+++ src/main/java/com/example/game/figures/EnemyE.java
@@ -1,16 +1,22 @@
 package com.example.game.figures;
 
+import com.example.game.main.Main;
+import com.example.game.main.Screen;
+import com.example.game.objects.MobInterface;
+
 import javax.swing.*;
 
 import java.util.List;
 
 
 public enum EnemyE {
-    AFFE(120, 190, 3,  2, MobMoveE.BMOVE,"src/main/resources/com/example/game/enemy/Affe2.png"),
-    UNICORN(64, 64, 2 , 4, MobMoveE.VERMOVE, "src/main/resources/com/example/game/enemy/Test.png"),
+
+
+    AFFE(120, 190, 3,  2, sinMove,"src/main/resources/com/example/game/enemy/Affe2.png"),
+    UNICORN(64, 64, 2 , 4, MobInterface sinMove(),"src/main/resources/com/example/game/enemy/Test.png"),
     DWARF(75, 125, 1, 3, MobMoveE.RAINBOW, "src/main/resources/com/example/game/enemy/Zwerg.png"),
     PUFFY(50, 50, 1, 6, MobMoveE.NMOVE, "src/main/resources/com/example/game/enemy/Spaceship.png"),
-    KITTY(75, 75, 1, 10000, MobMoveE.LASER, "src/main/resources/com/example/game/enemy/Kitty.png"),
+    KITTY(75, 75, 1, 100, MobMoveE.LASER, "src/main/resources/com/example/game/enemy/Kitty.png"),
 
     ;
     /*
@@ -21,12 +27,56 @@ public enum EnemyE {
         Glückwunsch du hast Erfolgreich ein neuen Enemy erstellt.
      */
 
+    static final MobInterface sinMove = (x, y, spawnHight) -> {
+        x++;
+        return Math.sin(x / 100) * 100 + spawnHight;
+    };
+
+
+    static MobInterface Sinmove2 = (x,y,spawnHight) -> {
+        x++;
+        return Math.sin(x / 100) * 200 + spawnHight;
+    };
+
+
+    static MobInterface verfolgungPlayer = (x,y,spawnHight) -> {
+        x++;
+        Screen s = Main.getScreen();
+        int px = s.mainChar.getX();
+        int py = s.mainChar.getY()-Main.player.getWidth();
+        int steigung = 0;
+        if (x + 64 < px) {
+            steigung = 0;
+        } else  {
+            steigung = ((py - y) / (px - x));
+        }
+        return y - steigung;
+    };
+
+
+    static MobInterface sawMove = (x,y,spawnHight) -> {
+        x++;
+        double sb=Screen.getWidht()/5;
+
+        double b=(1000/sb)*0.001;
+
+        return Screen.getWidht()*(b-Math.round(b+1);
+    };
+
+    static MobInterface lineareBewegung = (x,y,spawnHight) -> {
+        x++;
+        return y;
+    };
+
+
     private final int width;
     private final int height;
     private final int speed;// Max speed = 100
     private final Icon skin;
     private final int hp;
-    private final MobMoveE move;
+
+
+    public final MobInterface name;
 
     public static int getSize(EnemyE e, Level l){
         switch (e){
@@ -39,14 +89,22 @@ public enum EnemyE {
         return 0;
     }
 
-     EnemyE(int width, int height, int hp, int speed,MobMoveE m, String skin) {
+     EnemyE(int width, int height, int hp, int speed, MobInterface name, String skin ) {
         this.width = width;
         this.height = height;
         this.skin = new ImageIcon(skin); // creates icon object from incoming path
         this.hp = hp;
-        this.move = m;
         this.speed = speed;
-    }
+         this.name = name;
+     }
+
+
+
+
+
+
+
+
 
     public int getSpeed() {return speed;}
     public int getHp() {return hp;}
