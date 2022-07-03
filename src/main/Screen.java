20725fe78e1feb0a3f1b3405ad514b8570ed1570
Adni <adnibulliq@gmail.com>
2022-07-03 13:45:37 +0200

    add home screen and method in screen

--- src/main/Screen.java
+++ src/main/Screen.java
@@ -8,7 +8,14 @@ import java.awt.*;
 
 public class Screen extends JFrame {
 
-    private Tag score, lives, time, level;
+
+
+    private Tag score;
+    private Tag lives;
+    private Tag time;
+    private Tag level;
+
+
 
     private JLayeredPane layeredPane;
 
@@ -23,6 +30,7 @@ public class Screen extends JFrame {
 
         //initial settings for the window
         autoUpdateScreenDimensions();
+        // this.setUndecorated(true); //TODO: Full screen
         this.setTitle("Blank");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(true); //TODO: Wenn der Monitor zu klein gezogen wird, soll eine Nachricht kommen, die den User auffordert das Fenster größer zu ziehen
@@ -45,7 +53,8 @@ public class Screen extends JFrame {
         Keyboard k = Keyboard.getInstance();
         this.addKeyListener(k);
 
-        this.setVisible(true);
+        this.setVisible(false);
+
 
     }
 
@@ -76,6 +85,15 @@ public class Screen extends JFrame {
         System.out.println("Screen Dimensions: " + monitorWidth + "  " + monitorHeight);
     }
 
+    @Override
+    public JLayeredPane getLayeredPane() {
+        return layeredPane;
+    }
+
+    @Override
+    public void setLayeredPane(JLayeredPane layeredPane) {
+        this.layeredPane = layeredPane;
+    }
 
     public int getMonitorHeight() {
         return monitorHeight;
@@ -83,4 +101,37 @@ public class Screen extends JFrame {
     public int getMonitorWidth() {
         return monitorWidth;
     }
+    public Tag getScore() {
+        return score;
+    }
+
+    public void setScore(Tag score) {
+        this.score = score;
+    }
+
+    public Tag getLives() {
+        return lives;
+    }
+
+    public void setLives(Tag lives) {
+        this.lives = lives;
+    }
+
+    public Tag getTime() {
+        return time;
+    }
+
+    public void setTime(Tag time) {
+        this.time = time;
+    }
+
+    public Tag getLevel() {
+        return level;
+    }
+
+    public void setLevel(Tag level) {
+        this.level = level;
+    }
+
+
 }
